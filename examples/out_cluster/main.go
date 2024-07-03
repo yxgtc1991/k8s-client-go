f81d5860596f3041797c2b5a8a3381d209e239f8
yangxiaoguang <yangxiaoguang@cmss.chinamobile.com>
2024-07-03 17:54:07 +0800

    client-go 集群外认证配置

---
+++ examples/out_cluster/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"log"
+	"path/filepath"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	// 1、获取 kubeconfig 路径
+	homePath := homedir.HomeDir()
+	if homePath == "" {
+		log.Fatalf("failed to get the home directory")
+	}
+	kubeconfig := filepath.Join(homePath, ".kube", "config")
+	log.Printf("kube config: %s", kubeconfig)
+	// 2、通过 kubeconfig 初始化 config：kubeconfig 已包含 API Server 的连接信息
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		namespace := "default"
+		ctx := context.Background()
+		pods, err := clientset.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		for i, pod := range pods.Items {
+			log.Printf("%d -> %s/%s", i+1, pod.Namespace, pod.Name)
+		}
+		<-time.Tick(5 * time.Second)
+	}
+}
